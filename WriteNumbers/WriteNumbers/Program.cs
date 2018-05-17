66efe272575d70e6c78fc96bcd177a0899c2b4d1
chenmeiyi <a4527943@qq.com>
2018-05-17 17:23:49 +0800

    print 1 to 100 numbers

---
+++ WriteNumbers/WriteNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WriteNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 100; i++)
+            {
+                var thread = new Thread(WriteNumber);
+                thread.Start(i);
+            }
+            Console.WriteLine("Main Thread");
+            Console.ReadLine();
+        }
+
+        static void WriteNumber(Object number)
+        {
+            int i = 0;
+            try
+            {
+                i = (int)number;
+            }
+            catch(Exception e)
+            {
+                i = 9999;
+                Console.WriteLine("Not a correct number");
+            }
+            Console.WriteLine("Write number: " + i);
+        }
+    }
+}
